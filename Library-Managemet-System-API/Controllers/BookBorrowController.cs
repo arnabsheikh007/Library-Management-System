6ee806f4d9b7958fa828000dd7ca3d1ae48eb03d
Sheikh Bahauddin Arnab <arnab.sheikh007@gmail.com>
2024-05-08 20:23:53 +0600

    Added BookBorrow Controller

---
+++ Library-Managemet-System-API/Controllers/BookBorrowController.cs
@@ -0,0 +1,46 @@
+﻿using Library_Managemet_System_API.Models;
+using Library_Managemet_System_API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library_Managemet_System_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookBorrowController : ControllerBase
+    {
+        private readonly IBorrowedBookRepository _borrowedBookRepository;
+
+        public BookBorrowController(IBorrowedBookRepository borrowedBookRepository)
+        {
+            _borrowedBookRepository = borrowedBookRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<BorrowedBook> GetHistory()
+        {
+            return _borrowedBookRepository.GetAllBorrowedBooks();
+        }
+
+        [HttpGet]
+        [Route("{borrowId}")]
+        public BorrowedBook GetHistoryById(int borrowId)
+        {
+            return _borrowedBookRepository.GetBorrowedBookById(borrowId);
+        }
+
+        [HttpPost]
+        public IActionResult BorrowBook(BorrowedBook borrowedBook)
+        {
+            _borrowedBookRepository.AddBorrowedBook(borrowedBook);
+            return StatusCode(StatusCodes.Status201Created);
+        }
+        [HttpGet]
+        [Route("return/{borrowId}")]
+        public IActionResult ReturnBook(int borrowId)
+        {
+            _borrowedBookRepository.DeleteBorrowedBook(borrowId);
+            return StatusCode(StatusCodes.Status200OK);
+        }
+    }
+}
